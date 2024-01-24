506795c493f8c7d8a134b869729065824372bbfd
voidlighter <voidlighter@gmail.com>
2024-01-23 20:07:44 -0700

    Cleaned up all but pawn

--- shared/src/main/java/chess/movesets/BishopMoves.java
+++ shared/src/main/java/chess/movesets/BishopMoves.java
@@ -6,15 +6,7 @@ import chess.ChessPosition;
 
 public class BishopMoves extends Moveset {
     public boolean[][] getMoves(ChessBoard board, ChessPosition piecePosition) {
-        boolean[][] moves = new boolean[8][8];
-        int row = piecePosition.getRow() - 1;
-        int col = piecePosition.getColumn() - 1;
-
-        boolean isWhite = board.getPiece(piecePosition).getTeamColor() == ChessGame.TeamColor.WHITE;
-
         int[][] directions = new int[][]{{1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
-        moves = lineMoves(board, piecePosition, directions);
-
-        return moves;
+        return lineMoves(board, piecePosition, directions);
     }
 }
--- shared/src/main/java/chess/movesets/KnightMoves.java
+++ shared/src/main/java/chess/movesets/KnightMoves.java
@@ -6,16 +6,8 @@ import chess.ChessPosition;
 
 public class KnightMoves extends Moveset {
     public boolean[][] getMoves(ChessBoard board, ChessPosition piecePosition) {
-        boolean[][] moves = new boolean[8][8];
-        int row = piecePosition.getRow() - 1;
-        int col = piecePosition.getColumn() - 1;
-
-        boolean isWhite = board.getPiece(piecePosition).getTeamColor() == ChessGame.TeamColor.WHITE;
-
         int[][] directions = new int[][]{{1,2},{2,1},{2,-1},{1,-2},{-1,2},{-2,1},{-2,-1},{-1,-2}};
         int distance = 1;
-        moves = lineMoves(board, piecePosition, directions, distance);
-
-        return moves;
+        return lineMoves(board, piecePosition, directions, distance);
     }
 }
