b42f6e0e82726ef2358a999cdec343cadbe115a3
voidlighter <voidlighter@gmail.com>
2024-01-23 20:00:44 -0700

    Oh dang that worked for the Knight too

--- shared/src/main/java/chess/movesets/KnightMoves.java
+++ shared/src/main/java/chess/movesets/KnightMoves.java
@@ -1,14 +1,21 @@
 package chess.movesets;
 
 import chess.ChessBoard;
+import chess.ChessGame;
 import chess.ChessPosition;
 
 public class KnightMoves extends Moveset {
     public boolean[][] getMoves(ChessBoard board, ChessPosition piecePosition) {
-        boolean[][] validMoves = new boolean[8][8];
+        boolean[][] moves = new boolean[8][8];
+        int row = piecePosition.getRow() - 1;
+        int col = piecePosition.getColumn() - 1;
 
-        throw new UnsupportedOperationException("Implement Knight Moves!");
+        boolean isWhite = board.getPiece(piecePosition).getTeamColor() == ChessGame.TeamColor.WHITE;
 
-//        return validMoves;
+        int[][] directions = new int[][]{{1,2},{2,1},{2,-1},{1,-2},{-1,2},{-2,1},{-2,-1},{-1,-2}};
+        int distance = 1;
+        moves = lineMoves(board, piecePosition, directions, distance);
+
+        return moves;
     }
 }
